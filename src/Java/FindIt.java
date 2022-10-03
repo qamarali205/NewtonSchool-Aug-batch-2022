640d447785a97a85219eaa4285adddb20387f6f3
Qamar Ali <qamarali205@gmail.com>
2022-10-03 11:15:27 +0530

    update

--- src/Java/FindIt.java
+++ src/Java/FindIt.java
@@ -3,27 +3,19 @@ import java.util.*; // contains Collections framework
 
 // don't change the name of this class
 // you can add inner classes if needed
-class Find {
-    public static void numberMatches(long k) {
-        long start=1;
-        long last=(int)Math.sqrt(k);
-        while(start<=last){
-            long x=start+(last-start)/2;
-            if(k==(x*x)+(3*x)){
-                System.out.print(x);
-                return;
+class FindIt {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long ans = -1;
+        long sqrt = (long) Math.sqrt(n); //O(sqrt(n)) binary long
+        for (long i = 1; i <= sqrt; i++) {
+            long curr = (i * i) + (3 * i);
+            if (curr == n) {
+                ans = i;
+                break;
             }
-            else if(k<(x*x)+(3*x)){  // K=x^2+3x find x k given here in input
-                last=x-1;
-            }
-            else start=x+1;
         }
-        System.out.print("-1");
-    }
-    public static void main (String[] args) {
-        Scanner sc=new Scanner(System.in);
-        long k=sc.nextLong();
-        numberMatches(k);               
-                    
+        System.out.println(ans);
     }
 }
