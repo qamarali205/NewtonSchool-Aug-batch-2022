f66885c80747c218615829aec9c56d3f81a49d9c
Qamar Ali <qamarali205@gmail.com>
2022-10-06 11:26:51 +0530

    Bit Manipulation

---
+++ src/Java/KthBitSet.java
@@ -0,0 +1,26 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void isKthBitSet(long n, long k)
+    {
+        if ((n & (1 << (k - 1))) > 0)
+            System.out.println("SET");
+        else
+            System.out.println("NOT SET");
+    }
+    public static void main (String[] args) {
+                      // Your code here
+    Scanner sc=new Scanner(System.in);
+    long t=sc.nextLong();
+    while(t-->0){
+        long n=sc.nextLong();
+        long k=sc.nextLong();
+
+        isKthBitSet(n, k);
+    }
+
+}
+}
