0a59281eaaa9004be353f94a75de3c04d37eefb1
Qamar Ali <qamarali205@gmail.com>
2022-10-08 14:06:34 +0530

    Bit manipulation

--- src/Java/Binaryrepresentation.java
+++ src/Java/Binaryrepresentation.java
@@ -18,3 +18,25 @@ class Main {
         bin(n);
     }
 }
+
+//another method
+// import java.io.*; // for handling input/output
+// import java.util.*; // contains Collections framework
+
+// // don't change the name of this class
+// // you can add inner classes if needed
+// class Main {
+//      static void binary(long n)
+//     {
+//         if (n > 1)
+//             binary(n /2);
+ 
+//         System.out.print(n%2);
+//     }
+//     public static void main (String[] args) {
+//                       // Your code here
+//         Scanner sc=new Scanner(System.in);
+//         long n=sc.nextLong();
+//         binary(n);
+//     }
+// }
