52d189ae8101d0d07e25bdf9cc602a603d715acf
Qamar Ali <qamarali205@gmail.com>
2022-10-09 15:36:48 +0530

    Simple Sorting

---
+++ src/Java/SelectionSortSimple.java
@@ -0,0 +1,38 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    static void SelectionSort(int arr[], int n){
+        for(int i=0;i<n;i++){ 
+            int min_index=i;
+            for(int j=i+1; j<n; j++)
+                if(arr[j]<arr[min_index])
+                min_index=j;
+                
+                int temp=arr[min_index];
+                arr[min_index]=arr[i];
+                arr[i]=temp;
+                
+            
+           
+        }
+        }
+        
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        int arr[]=new int[n];
+        
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        SelectionSort(arr,n);
+        
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+        
+    }
+}
