ff5fbf92aa0a59dc4c5fefd8ec6e5aee8ee05599
Qamar Ali <qamarali205@gmail.com>
2022-10-12 11:07:11 +0530

    Magic Number Loops

--- src/Java/magicnumber.java
+++ src/Java/magicnumber.java
@@ -1,55 +1,29 @@
-static int MagicNumber(int N){
-    //Enter your code here
-    if(N<=88){
-        return 79;
+import java.util.*;
+public class MagicNumber {
+    static boolean isMagic(int n) {
+        boolean nine = false, seven = false;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            n = n / 10;
+            if (lastDigit == 9) nine = true;
+            else if (lastDigit == 7) seven = true;
+        }
+        if (seven && nine) return true;
+        return false;
     }
-    int number1=N, number2=N;
-    int ans1=0, ans2=0;
-    while(number1>0)
-    {
-        int n=number1;
-        int seven=0, nine=0, ex=0;
-        while(n>0)
-        {
-            if(n%10==7)
-            seven=1;
-            if(n%10==9)
-            nine=1;
-            if(nine==1 && seven==1){
-                ans1=number1;
-                ex=1;
-                break;
+
+    static int MagicNumber(int N) {
+
+        int forward = N, backward = N;
+        while (true) {
+            if (isMagic(backward)) {
+                return backward;
+            } else if (isMagic(forward)) {
+                return forward;
             }
-            n/=10;
+            forward++;
+            backward--;
         }
-        if(ex==1)
-        break;
-        number1--;
-        }
-        while(number2>0)
-        {
-            int n=number2;
-            int seven=0,nine=0, ex=0;
-            while(n>0){
-                if(n%10==7)
-                seven=1;
-                if(n%10==9)
-                nine=1;
-                if(nine==1 && seven==1){
-                    ans2=number2;
-                    ex=1;
-                    break;
-                }
-                    n/=10;
-                 }
-                 if(ex==1)
-                 break;
-                 number2++;
-                 }
-                 int l,r;
-                 l=N-ans1;
-                 r=ans2-N;
-                 if(l<=r)
-                 return ans1;
-                 return ans2;
-        }
+
+    }
+}
