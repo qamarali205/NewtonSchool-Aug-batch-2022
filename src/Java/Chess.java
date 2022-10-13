a7f74ce759729bcdea7ee3dc205fbd9fb0170afe
Qamar Ali <qamarali205@gmail.com>
2022-10-13 20:22:09 +0530

    Chess Board

--- src/Java/Chess.java
+++ src/Java/Chess.java
@@ -3,36 +3,20 @@ import java.util.*; // contains Collections framework
 
 // don't change the name of this class
 // you can add inner classes if needed
-class Chess {
-   public static void main (String[] args) throws Exception{
-                      // Your code here
-                      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                      int n = Integer.parseInt(br.readLine());
-                      StringBuilder sb = new StringBuilder();
-                      int[][] a=new int[n][n];
-                      
-                      int i,j,c1=0,c2=0;
-                      if(n>=1 && n<=1000){
-                      for(i=0;i<n;i++)
-                      {
-                           String[] line = br.readLine().split(" ");
-                          for(j=0;j<n;j++){
-                              a[i][j]=Integer.parseInt(line[j]);       
-                              int x=(i+j)&1;
-                              if(a[i][j]==x)
-                                     c1+=1;        
-                              else
-                                   c2+=1;
-                             }
-                      }
-                      if(c1>=c2)
-                      sb.append(c2);
-    
-                      else
-                      sb.append(c1);
-
-                      System.out.print(sb.toString());
-                  
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int[][] arr = new int[n][n];
+        int match = 0, notMatch = 0;
+        for (int i = 0; i < n; i++) {
+            String row[] = sc.nextLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                int value = (i + j) % 2;
+                if (value == Integer.parseInt(row[j])) match++;
+                if (value != Integer.parseInt(row[j])) notMatch++;
+            }
+        }
+        System.out.println(Math.min(match, notMatch));
     }
-}
 }
