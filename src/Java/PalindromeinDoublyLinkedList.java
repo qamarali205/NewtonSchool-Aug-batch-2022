dd14c45cdd9e26cac28e304dea72f51c0f534a4e
Qamar Ali <qamarali205@gmail.com>
2022-10-18 11:00:32 +0530

    Doubly link list

---
+++ src/Java/PalindromeinDoublyLinkedList.java
@@ -0,0 +1,29 @@
+/*
+class Node
+{
+   char data;
+   Node next, prev;
+};
+*/
+ static boolean isPalin( Node left)
+    {
+        // Write your code here
+        if (left == null)
+    return true;
+ 
+    // Find rightmost node
+    Node right = left;
+    while (right.next != null)
+        right = right.next;
+ 
+    while (left != right)
+    {
+        if (left.data != right.data)
+            return false;
+ 
+        left = left.next;
+        right = right.prev;
+    }
+ 
+    return true;
+    }
