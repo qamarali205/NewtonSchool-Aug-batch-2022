9d2e3d67b7ebaf175a024955956344ed23f6e3fc
Qamar Ali <qamarali205@gmail.com>
2022-10-25 12:06:18 +0530

    Circular Link List

---
+++ src/Java/ExchangeNodes.java
@@ -0,0 +1,31 @@
+/*
+class Node {
+    Node next;
+    int val;
+
+    Node(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+*/
+public static Node exchangeNodes(Node head) {
+//Enter your code here
+    
+       if (head.next.next == head) {
+            head = head.next;
+            return head;
+        }
+      
+        Node p = head;
+        while (p.next.next != head)
+            p = p.next;
+ 
+    
+        p.next.next = head.next;
+        head.next = p.next;
+        p.next = head;
+        head = head.next;
+ 
+        return head;
+}
