295d2b3a045626e0e67c1e62b5c13cf349f9e0a8
Qamar Ali <qamarali205@gmail.com>
2022-10-27 13:23:13 +0530

    Arena: Delete the Kth node from the end

---
+++ src/Java/DeletetheKthnodefromtheend.java
@@ -0,0 +1,34 @@
+/*
+class Node {
+    Node next;
+    int val;
+
+    Node(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+*/
+
+public static Node deleteElement(Node head,int k) {
+//enter your code here
+      int c=0;
+        Node temp=head,old=null;
+
+        while(temp!=null){
+            temp=temp.next;
+            c++;
+        }
+        
+        temp=head;
+        for(int i=1;i<=c-k;i++){
+            old=temp;
+            temp=temp.next;
+        }
+        if(temp==head)
+           head=head.next;
+        else
+         old.next=temp.next;
+
+        return head;
+    }Link 
