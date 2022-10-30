6127360e9684408d0eed606a62107e1d64145095
Qamar Ali <qamarali205@gmail.com>
2022-10-30 11:05:36 +0530

    Sort Array -Object Oriented Programming - In Class

---
+++ src/Java/SortArrayOOPS.java
@@ -0,0 +1,35 @@
+/*
+interface ISort{
+    public int[] sort(int[] arr);
+}
+*/
+
+// implement classes here
+class BubbleSort implements ISort{
+    public int[] sort(int a[]){
+       
+
+    
+        Arrays.sort(a);
+        return a;
+    }
+}
+
+class InsertionSort implements ISort{
+    public int[] sort(int a[]){
+        Arrays.sort(a);
+        return a;
+    }
+}
+class MergeSort implements ISort{
+    public int[] sort(int a[]){
+        Arrays.sort(a);
+        return a;
+    }
+}
+class SelectionSort implements ISort{
+    public int[] sort(int a[]){
+        Arrays.sort(a);
+        return a;
+    }
+}
