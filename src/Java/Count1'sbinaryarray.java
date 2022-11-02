0abb24ad4558b88e544fa5619d0961563e043354
Qamar Ali <qamarali205@gmail.com>
2022-11-02 11:41:40 +0530

    Arena question - Count 1's in binary array

---
+++ src/Java/Count1'sbinaryarray.java
@@ -0,0 +1,27 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+
+            for(int i=0; i<n; i++){
+                arr[i]=sc.nextInt();
+            }
+            int count=0;
+            for(int i=0; i<n; i++){
+                if(arr[i]==1){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
