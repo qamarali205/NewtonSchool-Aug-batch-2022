4cb6ec2b5d469bcc8b282da4398baf3ea304a6c5
Qamar Ali <qamarali205@gmail.com>
2022-11-07 11:46:04 +0530

    Hashmap basic

---
+++ src/Java/HashMapbasic.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HashMapbasic {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm=new HashMap<>();
+        
+        hm.put("Java",1);
+        hm.put(".Net",2);
+        hm.put("Php",3);
+        hm.put("C lang",4);
+        
+        System.out.print(hm);
+    }
+}
