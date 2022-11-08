76334f68329d9cf105104f8caa40f09185fca878
Qamar Ali <qamarali205@gmail.com>
2022-11-08 10:23:22 +0530

    OOPS

---
+++ src/OOPS/SingleInheritance.java
@@ -0,0 +1,44 @@
+class Vehicle { //parent super base
+    static int noWheels;
+    final int num = 10; // constant
+    private String model;
+    protected String brand;
+
+}
+
+class Car extends Vehicle { // child subclass derived
+    int noOfSeats;
+}
+
+class RaceCar extends Car {
+    //
+}
+
+class TwoWheeler extends Vehicle {
+    int noWheels = 2;
+
+    static class AB {
+
+    }
+}
+
+
+public class SingleInheritance {
+    void hi() {
+        System.out.println("hi");
+    }
+
+    public static void main(String[] args) {
+        Car v1 = new Car();
+        v1.noWheels = 10;
+        Car v2 = new Car();
+        v2.noWheels = 20;
+//
+//        System.out.println(Vehicle.model); // 10
+//        System.out.println(Vehicle.noWheels); //20
+////        Car v2 = new Car();
+////        c1.noOfSeats = 10;
+////        c1.brand = "abcd";
+    }
+
+}
