6de4711d6140b7fd3223f6e0b3e51ec30846feca
Qamar Ali <qamarali205@gmail.com>
2022-11-08 10:26:29 +0530

    OOPS

---
+++ src/OOPS/Student.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Student {
+    int rollNo;
+
+    @java.lang.Override
+    public String toString() {
+        return "Student{" + "rollNo=" + rollNo + ", name='" + name + '\'' + '}';
+    }
+
+    String name;
+
+    public Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+
+}
