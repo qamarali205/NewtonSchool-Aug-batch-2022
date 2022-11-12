342a0a6c54d0f989b61c32d13b963cfff674909d
Qamar Ali <qamarali205@gmail.com>
2022-11-12 21:30:43 +0530

    Stack,Queue and Deque

--- src/Java/BobandArray.java
+++ src/Java/BobandArray.java
@@ -4,63 +4,26 @@ import java.util.*; // contains Collections framework
 // don't change the name of this class
 // you can add inner classes if needed
 class Main {
-       public int front;
-       public int rear;
-       public  int cap;
-       public int arr[];
-
-    
-        Main(int n){
-        cap=1000000;
-        arr = new int[cap];
-        front=cap/2;
-        rear=cap/2;
-        }
-        public void add(int x){
-            
-           if(rear==cap/2){
-               
-             arr[rear]=x;
-             rear++;
-             return;
-
-           }
-            if(arr[front]>x){
-                arr[rear]=x;
-                rear++;
-            }
-            
-            else{
-                front--;
-                arr[front]=x;
-                
-            }
-            }
-            public void display(){
-                for(int i = front;i<rear;i++){
-                    System.out.print(arr[i]+" ");
-                }
-            } 
-
-    
-
-   
     public static void main (String[] args) {
-                      // Your code here
-                      Scanner sc = new Scanner(System.in);
-                      int n = sc.nextInt();
-                      int arr[] = new int[n];
-
-                      for(int i=0;i<n;i++){
-                          arr[i]=sc.nextInt();
-                      }
-
-                      Main st;
-                      st = new Main(n);
-                      for(int i=0;i<n;i++){
-                          st.add(arr[i]);
-                      }
-                      st.display();
-
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        Deque<Integer> dq= new LinkedList<>();
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        dq.addFirst(arr[0]);
+        for(int i=1; i<n; i++){
+            if(arr[i]>=dq.element()){
+                dq.addFirst(arr[i]);
+            }
+            else{
+                dq.addLast(arr[i]);
+            }
+        }
+        while(dq.size()!=0){
+            System.out.print(dq.poll()+" ");
+        }
+        
     }
 }
