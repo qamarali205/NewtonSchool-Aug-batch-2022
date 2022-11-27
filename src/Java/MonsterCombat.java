5d0b99903dcef93b11f359c9fb31f0e8bc9210c3
Qamar Ali <qamarali205@gmail.com>
2022-11-27 16:24:01 +0530

    Weekend Contest - [27.11.2022]

--- src/Java/MonsterCombat.java
+++ src/Java/MonsterCombat.java
@@ -9,6 +9,7 @@ class Main {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int arr[]=new int[n];
+        int r=0;
 
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
@@ -32,3 +33,4 @@ class Main {
 
     }
 }
+///////hheeh
